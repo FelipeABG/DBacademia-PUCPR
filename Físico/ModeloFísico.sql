00b5e168ccc176e48c1ad35d49b29c2b16ff2fe6
Felipe <felipe.baleche2@gmail.com>
2023-09-11 15:16:54 -0300

    mudanças

--- Físico/ModeloFísico.sql
+++ Físico/ModeloFísico.sql
@@ -59,6 +59,20 @@ create table physical_assessment(
     foreign key(employee_id) references employee(id)
 );
 
+create table purchase(
+    id int primary key auto_increment not null,
+    price decimal(8,2) not null,
+    customer_id int,
+    foreign key(customer_id) references customer(id)
+);
+
+create table class(
+    id int primary key auto_increment not null,
+    class_datetime datetime not null,
+    class_type enum('spinning', 'zumba', 'pilates') not null,
+    price decimal(5,2) not null,
+)
+
 create table billing(
     id int primary key auto_increment not null,
     payment_method enum('credit card', 'debit card', 'pix') not null,
@@ -66,5 +80,28 @@ create table billing(
     billing_datetime datetime not null,
     due_datetime datetime not null,
     employee_id int,
+    class_id int,
+    purchase_id int,
+    membership_id int,
+    foreign key(employee_id) references employee(id),
+    foreign key(class_id) references class(id),
+    foreign key(purchase_id) references purchase(id),
+    foreign key(membership_id) references membership(id)
+);
 
+create table products(
+    id int primary key auto_increment not null,
+    product_name varchar(256) not null,
+    product_type enum('suplemento', 'comida', 'bebida') not null,
+    price decimal(8,2) not null,
+    amount smallint(5) not null
+);
+
+create table membership(
+    id int primary key auto_increment not null,
+    price decimal(8,2) not null,
+    membership_name varchar(45) not null,
+    membership_type enum('mensal', 'trimestral', 'semestral', 'anual') not null,
+    customer_id int,
+    foreign key(customer_id) references customer(id)
 );
