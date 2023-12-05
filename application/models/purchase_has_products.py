658cb111c91aa2654cab538600fd2a27ab563271
Felipe <felipe.baleche2@gmail.com>
2023-12-05 17:19:12 -0300

    removed application

--- application/models/purchase_has_products.py
+++
@@ -1,9 +0,0 @@
-from models import Base, Purchase, Products
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, Integer
-
-class PurchaseHasProducts(Base):
-    __tablename__ = 'purchase_has_products'
-    
-    purchase_id: Mapped[int] = mapped_column('purchase_id', Integer(), ForeignKey(Purchase.id), primary_key=True, nullable=False)
-    product_id:  Mapped[int] = mapped_column('product_id', Integer(), ForeignKey(Products.id), primary_key=True, nullable=False)
